6bd7576632f2e78e9c6335e67688eab9c9d3d301
Mariette Schonfeld <marietteschonfeld@gmail.com>
2021-12-08 15:33:52 +0100

    implemented a python script to generate random graphs.

---
+++ exercise3/generate_graph.py
@@ -0,0 +1,31 @@
+from networkx.generators.random_graphs import erdos_renyi_graph
+import random
+
+seed = 8122021
+
+# check if every node has at least one edge by flattening the list of edge tuples,
+# convert it to a set and see if it as long as the node list.
+def is_connected(graph):
+    return len(set((sum(graph.edges, ())))) == len(graph.nodes)
+
+def create_graph(n):
+    # p value dictates the sparsity of the graph
+    p = random.uniform(0.25, 0.75)
+    g = erdos_renyi_graph(n, p, seed=seed, directed=False)
+    while not is_connected(g):
+        p = random.uniform(0.25, 0.75)
+        g = erdos_renyi_graph(n, p, seed=seed, directed=False)
+    return g
+
+n = 10
+graph = create_graph(n)
+name = "test"
+
+with open("graphs/graph_{}.txt".format(name), 'w') as f:
+    f.write(str(n))
+    f.write('\n')
+    f.write(str(len(graph.edges)))
+    f.write('\n')
+    for line in graph.edges:
+        f.write(str(line))
+        f.write('\n')
